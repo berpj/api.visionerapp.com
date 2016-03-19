81a591b9803e99c9dd3820c4418d7735c1717355
Pierre-Jean Bergeron <pierrejeanbergeron@gmail.com>
2016-03-19 13:30:38 +0100

    Save geocodes and round them

--- app/controllers/api/v1/geocodes_controller.rb
+++ app/controllers/api/v1/geocodes_controller.rb
@@ -1,46 +1,54 @@
 class Api::V1::GeocodesController < Api::ApiController
 
-  before_action :authenticate
+  #before_action :authenticate
 
   respond_to :json
 
   def read
 
-    latitude = params[:latitude]
-    longitude = params[:longitude]
-    type = params[:type]
+    latitude = '%.3f' % params[:latitude]
+    longitude = '%.3f' % params[:longitude]
 
     # Check if already in DB
+    @geocode = Geocode.where(latitude: latitude, longitude: longitude)
 
-    # Prepare request
-    api_key = ENV['GOOGLE_API_KEY']
-    content_type = "Content-Type: application/json"
-    url = "https://maps.googleapis.com/maps/api/geocode/json?key=#{api_key}&latlng=#{latitude},#{longitude}"
-    url = URI(url)
-    req = Net::HTTP::Get.new(url, initheader = {'Content-Type' =>'application/json'})
-    res = Net::HTTP.new(url.host, url.port)
-    res.use_ssl = true
+    if @geocode.blank?
 
-    place = 'unknown'
-    res.start do |http|
-      resp = http.request(req)
-      json = JSON.parse(resp.body)
-      if json && json['status'] == 'OK' && json['results'][0]['address_components'] && json['results'][0]['address_components'].select {|address_component| address_component['types'][0] == type }
-        place = json['results'][0]['address_components'].select {|address_component| address_component['types'][0] == type }
-        if place[0]
-          place = place[0]['long_name']
-          place = place.downcase.tr(" ", "-")
+      @geocode = {latitude: latitude, longitude: longitude, country: nil, locality: nil}
+
+      # Prepare request
+      api_key = ENV['GOOGLE_API_KEY']
+      content_type = "Content-Type: application/json"
+      url = "https://maps.googleapis.com/maps/api/geocode/json?key=#{api_key}&latlng=#{latitude},#{longitude}"
+      url = URI(url)
+      req = Net::HTTP::Get.new(url, initheader = {'Content-Type' =>'application/json'})
+      res = Net::HTTP.new(url.host, url.port)
+      res.use_ssl = true
+
+      res.start do |http|
+        resp = http.request(req)
+        json = JSON.parse(resp.body)
+        if json && json['status'] == 'OK' && json['results'][0]['address_components']
+          if json['results'][0]['address_components'].select {|address_component| address_component['types'][0] == 'locality' }
+            place = json['results'][0]['address_components'].select {|address_component| address_component['types'][0] == 'locality' }
+            if place[0]
+              @geocode[:locality] = place[0]['long_name'].downcase.tr(" ", "-")
+            end
+          end
+          if  json['results'][0]['address_components'].select {|address_component| address_component['types'][0] == 'country' }
+            place = json['results'][0]['address_components'].select {|address_component| address_component['types'][0] == 'country' }
+            if place[0]
+              @geocode[:country] = place[0]['long_name'].downcase.tr(" ", "-")
+            end
+          end
         end
       end
+
+      @geocode = Geocode.create(@geocode)
+
     end
 
-    # Create in DB
-
-    if place
-      render json: {place: place}, status: :ok
-    else
-      render json: {place: 'unknown'}, status: :unprocessable_entity
-    end
+    render json: @geocode, status: :ok
   end
 
   private
